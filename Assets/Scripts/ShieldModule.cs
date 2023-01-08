2bce04cb98af06f23d88df72cf9f63fb1b46ef0c
Koekiemonster69 <85498854+DeSaaiman@users.noreply.github.com>
2023-01-08 22:54:22 +0100

    cummit

--- Assets/Scripts/ShieldModule.cs
+++ Assets/Scripts/ShieldModule.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class ShieldModule : Module
 {
-    // Start is called before the first frame update
-    protected override void Start()
-    {
-        base.Start();
-    }
+    
 
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
-    }
-
-    protected override void Die()
-    {
-        base.Die();
-    }
 }
