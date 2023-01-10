718e7b13ae73c2225e02aa4a72ec562427a0132f
Lars <l.j.vancroonenburg@students.uu.nl>
2023-01-10 17:57:27 +0100

    File cleanup (might lead to duplicate files / folders so be ware)

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -76,7 +76,7 @@ public class PlayerCar : HealthEntity
 
     private string[] ReadModulesFromChips() //TODO: read from chips
     {
-        return new string[] { "Gun", "Shield" };
+        return new string[] { "Gun", "SpeedAbility" };
     }
 
     // Update is called once per frame
