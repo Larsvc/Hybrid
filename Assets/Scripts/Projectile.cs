239f192dd55cd50bd8b816629ef6fc15ff92ff14
Lars <l.j.vancroonenburg@students.uu.nl>
2023-01-16 15:07:11 +0100

    i like all dit

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class Projectile : MonoBehaviour
 {
-    [HideInInspector] public LayerMask mask;
+    public LayerMask mask;
 
     [SerializeField] private float damage = 60f;
     [SerializeField] private float radius = 5f;
@@ -35,24 +35,7 @@ public class Projectile : MonoBehaviour
         {
             dead = true;
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-            List<HealthEntity> shouldBeDamaged = new List<HealthEntity>();
-
-            foreach (Collider c in colliders)
-            {
-                HealthEntity fp = FirstParent(c.transform);
-                if (fp && !shouldBeDamaged.Contains(fp))
-                {
-                    fp.TakeHit(damage, hitmarkerFromPlayer);
-                    shouldBeDamaged.Add(fp);
-                }
-                    
-                /*bool canDamage = FirstParent(collision.collider.transform);
-                if (canDamage)
-                {
-                    FirstParent(collision.collider.transform).TakeHit(damage, hitmarkerFromPlayer);
-                }*/
-            }
+            AreaDamage();
 
             GameObject explosion = Instantiate(PrefabManager.instance.cannonballExplosion, transform.position, Quaternion.identity);
             Destroy(explosion, 2f);
@@ -66,13 +49,22 @@ public class Projectile : MonoBehaviour
         {
             dead = true;
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-            List<HealthEntity> shouldBeDamaged = new List<HealthEntity>();
+            AreaDamage();
 
-            foreach (Collider c in colliders)
+            GameObject explosion = Instantiate(PrefabManager.instance.cannonballExplosion, transform.position, Quaternion.identity);
+            Destroy(explosion, 2f);
+            Destroy(gameObject);
+        }
+    }
+
+    private void AreaDamage()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        List<HealthEntity> shouldBeDamaged = new List<HealthEntity>();
+        foreach (Collider c in colliders)
             {
                 HealthEntity fp = FirstParent(c.transform);
-                if (!shouldBeDamaged.Contains(fp))
+                if (fp && !shouldBeDamaged.Contains(fp))
                 {
                     fp.TakeHit(damage, hitmarkerFromPlayer);
                     shouldBeDamaged.Add(fp);
@@ -84,11 +76,6 @@ public class Projectile : MonoBehaviour
                     FirstParent(collision.collider.transform).TakeHit(damage, hitmarkerFromPlayer);
                 }*/
             }
-
-            GameObject explosion = Instantiate(PrefabManager.instance.cannonballExplosion, transform.position, Quaternion.identity);
-            Destroy(explosion, 2f);
-            Destroy(gameObject);
-        }
     }
 
     private HealthEntity FirstParent(Transform t)
